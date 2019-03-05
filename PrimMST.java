e8d8a2a8e60c36ba56670687e90967383d5a2e61
liulvjian <634098330@qq.com>
2019-03-05 20:55:10 +0800

    2019/03/05 home

---
+++ PrimMST.java
@@ -0,0 +1,51 @@
+public class PrimMST {
+  private Edge[] edgeTo;
+  private double[] distTo;
+  private boolean marked[];
+  private IndexMinPQ<Double> pq;
+
+  public PrimMST(EdgeWeightedGraph G) {
+    edgeTo = new Edge[G.V()];
+    distTo = new double[G.V()];
+    marked = new boolean[G.V()];
+    for (int v = 0; v < G.V(); v++) {
+      distTo[v] = Double.POSITIVE_INFINITY;
+    }
+    pq = new IndexMinPQ<Double>(G.V());
+
+    distTo[0] = 0.0;
+    pq.insert(0, 0.0);
+    while (!pq.isEmpty()) {
+      visit(G, pq.delMin());
+    }
+  }
+
+  private void visit(EdgeWeightedGraph G, int v) {
+    marked[v] = true;
+    for (Edge e : G.adj(v)) {
+      int w = e.other(v);
+      if (marked[w]) continue;
+      if (e.weight() < distTo[w]) {
+        edgeTo[w] = e;
+        distTo[w] = e.weight();
+        if (pq.contains(w)) pq.change(w, distTo[w]);
+        else pq.insert(w, distTo[w]);
+      }
+    }
+  }
+
+  public Iterable<Edge> edges() {
+    Queue<Edge> queue = new Queue<Edge>();
+    for(int v = 0; v < edgeTo.length; v++) {
+      Edge e = edgeTo[v];
+      if (e != null) queue.enqueue(e);
+    }
+    return queue;
+  }
+
+  public double weight() {
+    double weight = 0.0;
+    for (int w = 0; w < distTo.length; w++) weight += distTo[w];
+    return weight;
+  }
+}
